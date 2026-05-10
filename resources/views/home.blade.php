8c235c7c3f8ad768cf6b94d87a8e3c2d744525c8
ATHOS MELO DOS SANTOS <athos.santos@sou.ufac.br>
2026-05-10 17:30:34 -0300

    Inicialização Projeto Laravel TechStore

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>TechStore</title>
+</head>
+<body>
+
+    <h1>Bem-vindo à TechStore :D</h1>
+
+    <p>Minha loja de informática em Laravel 10.</p>
+    <p>Em breve construiremos esse site para ficar TOPZERO</p>
+
+</body>
+</html>
